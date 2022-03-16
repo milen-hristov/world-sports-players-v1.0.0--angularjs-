33ee71f27df41bb806e93ece6b7b8a4aa730bd5f
Milen Hristov <milenskibg88@gmail.com>
2022-03-16 17:45:49 +0200

    Added basic app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PlayersDetailsComponent } from './players/players-details/players-details.component';
+import { PlayersListComponent } from './players/players-list/players-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/players', pathMatch: 'full' },
+  {
+    path: 'players', component: PlayersListComponent, children: [
+      { path: ':id', component: PlayersDetailsComponent },
+    ]
+  },
+  { path: 'player-details', component: PlayersDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
