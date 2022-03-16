daa96a494b0f9c54b90d7ff3f322e6bca19e840b
Milen Hristov <milenskibg88@gmail.com>
2022-03-16 18:31:02 +0200

    Added basic Players Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PlayersComponent } from './players/players.component';
 import { PlayersListComponent } from './players/players-list/players-list.component';
 import { PlayersDetailsComponent } from './players/players-details/players-details.component';
+import { PlayersService } from './players/players.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { PlayersDetailsComponent } from './players/players-details/players-detai
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PlayersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
