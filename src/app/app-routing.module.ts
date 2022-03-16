35979e1c603e9124307fe5dc96c683c3aa6b5b5f
Milen Hristov <milenskibg88@gmail.com>
2022-03-16 20:34:32 +0200

    Optimised routes
    An idea to place the Player Details next to the Players List. TBC

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PlayersDetailsComponent } from './players/players-details/players-details.component';
+import { PlayersListComponent } from './players/players-list/players-list.component';
 import { PlayersComponent } from './players/players.component';
+
+// const routes: Routes = [
+//   { path: '', redirectTo: '/players', pathMatch: 'full' },
+//   {
+//     path: 'players', component: PlayersComponent, children: [
+//       { path: ':id', component: PlayersDetailsComponent },
+//     ]
+//   },
+// ];
 
 const routes: Routes = [
   { path: '', redirectTo: '/players', pathMatch: 'full' },
   {
     path: 'players', component: PlayersComponent, children: [
+      { path: '', component: PlayersListComponent },
       { path: ':id', component: PlayersDetailsComponent },
     ]
   },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
