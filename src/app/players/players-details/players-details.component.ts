becedcf6ba0d88fa1aa79b1813cd3d7d1d05e260
Milen Hristov <milenskibg88@gmail.com>
2022-03-19 20:48:08 +0200

    Remove error when loading player details

--- src/app/players/players-details/players-details.component.ts
+++ src/app/players/players-details/players-details.component.ts
@@ -1,66 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params, Router } from '@angular/router';
-import { AuthService } from 'src/app/auth/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { AuthService } from "src/app/auth/auth.service";
 
-import { Player } from '../player.model';
-import { PlayersService } from '../players.service';
+import { Player } from "../player.model";
+import { PlayersService } from "../players.service";
 
 @Component({
-  selector: 'app-players-details',
-  templateUrl: './players-details.component.html',
-  styleUrls: ['./players-details.component.css']
+  selector: "app-players-details",
+  templateUrl: "./players-details.component.html",
+  styleUrls: ["./players-details.component.css"],
 })
 export class PlayersDetailsComponent implements OnInit {
-  player: Player;
+  player: Player = {
+    id: "",
+    name: "",
+    description: "",
+    imagePath: "",
+    country: "",
+    dob: "",
+    achievements: "",
+    active: "",
+    sport: "",
+    owner: "",
+  };
+
   id: string;
 
   isOwner: boolean;
   ownerID: string;
   currentUserID: string;
 
-  constructor(private playersService: PlayersService,
+  constructor(
+    private playersService: PlayersService,
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService) {
-  }
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.id = params['id'];
-          this.playersService.getPlayer(this.id).subscribe((player) => {
-            this.player = player;
-            this.ownerID = player.owner;
+    this.route.params.subscribe((params: Params) => {
+      this.id = params["id"];
+      this.playersService.getPlayer(this.id).subscribe((player) => {
+        this.player = player;
+        this.ownerID = player.owner;
 
-            this.authService.user.subscribe(user => {
-              this.currentUserID = user.id;
-            });
+        this.authService.user.subscribe((user) => {
+          this.currentUserID = user.id;
+        });
 
-
-            if (this.ownerID === this.currentUserID) {
-              this.isOwner = true;
-            } else {
-              this.isOwner = false;
-            }
-          });
+        if (this.ownerID === this.currentUserID) {
+          this.isOwner = true;
+        } else {
+          this.isOwner = false;
         }
-      );
+      });
+    });
   }
 
   onEditPlayer() {
     // this.router.navigate(['edit'], { relativeTo: this.route });
-    this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route });
+    this.router.navigate(["../", this.id, "edit"], { relativeTo: this.route });
   }
 
   onDeletePlayer() {
     this.playersService.deletePlayer(this.id).subscribe({
       next: () => {
-        this.router.navigate(['/players']);
+        this.router.navigate(["/players"]);
       },
       error: (err) => {
         console.log(err);
-      }
+      },
     });
   }
 }
