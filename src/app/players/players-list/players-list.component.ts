d42b2c1a556413ef63409a8ce2f5ffa78cc0267f
Milen Hristov <milenskibg88@gmail.com>
2022-03-25 22:49:23 +0200

    Added player search

--- src/app/players/players-list/players-list.component.ts
+++ src/app/players/players-list/players-list.component.ts
@@ -1,40 +1,75 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { map } from "rxjs/operators";
+import { FormControl, FormGroup } from "@angular/forms";
 
 import { PlayersService } from "../players.service";
 import { Player } from "../player.model";
 import { HandleError } from "src/app/shared/handleError.service";
-import { animate, query, stagger, style, transition, trigger } from "@angular/animations";
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-players-list",
   templateUrl: "./players-list.component.html",
   styleUrls: ["./players-list.component.css"],
   animations: [
-    trigger('fade', [
-      transition('void => *', [
-        query(':enter', [
+    trigger("fade", [
+      transition("void => *", [
+        query(":enter", [
           style({
-            opacity: '0'
+            opacity: "0",
           }),
-          stagger(30, [
-            animate('300ms ease-in', style({ opacity: 1 }))
-          ])
-        ])
+          stagger(30, [animate("300ms ease-in", style({ opacity: 1 }))]),
+        ]),
       ]),
-      transition('* => void', animate('300ms ease-out', style({ opacity: 0 })))
-    ])
-  ]
+      transition("* => void", animate("300ms ease-out", style({ opacity: 0 }))),
+    ]),
+  ],
 })
-export class PlayersListComponent implements OnInit {
+export class PlayersListComponent implements OnInit, OnDestroy {
   players: Player[] | undefined;
+  playersBackup: Player[] | undefined;
   isLoading = false;
   message: string = null;
+  searchForm: FormGroup;
+  subscription: Subscription;
 
-  constructor(private playersService: PlayersService, private handleError: HandleError) {}
+  constructor(
+    private playersService: PlayersService,
+    private handleError: HandleError
+  ) {}
 
   ngOnInit() {
     this.fetchPlayers();
+    this.initForm();
+
+    this.subscription = this.searchForm.valueChanges.subscribe((searchObj) => {
+      this.players = this.playersBackup;
+      let search = searchObj.searchedText.trim();
+
+      let filterPlayers = this.players.filter(
+        (x) =>
+          x.name.toLowerCase().includes(search.toLowerCase()) ||
+          x.sport.toLowerCase().includes(search.toLowerCase()) ||
+          x.country.toLowerCase().includes(search.toLowerCase())
+      );
+      this.players = filterPlayers;
+    });
+  }
+
+  private initForm() {
+    let searchedText = "";
+
+    this.searchForm = new FormGroup({
+      searchedText: new FormControl(searchedText),
+    });
   }
 
   fetchPlayers() {
@@ -56,6 +91,7 @@ export class PlayersListComponent implements OnInit {
       .subscribe({
         next: (players) => {
           this.players = players;
+          this.playersBackup = players;
           this.isLoading = false;
           // console.log(this.players);
         },
@@ -63,7 +99,11 @@ export class PlayersListComponent implements OnInit {
           this.message = this.handleError.handleErrorPlayer(err);
           this.isLoading = false;
           console.log(err);
-        }
+        },
       });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
