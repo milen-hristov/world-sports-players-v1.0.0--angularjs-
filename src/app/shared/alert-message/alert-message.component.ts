9c000b53af790743bee446668afb45224ea48b1c
Milen Hristov <milenskibg88@gmail.com>
2022-04-03 18:08:21 +0300

    Updated alert component

--- src/app/shared/alert-message/alert-message.component.ts
+++ src/app/shared/alert-message/alert-message.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-alert-message',
   templateUrl: './alert-message.component.html',
   styleUrls: ['./alert-message.component.css'],
 })
-export class AlertMessageComponent {
+export class AlertMessageComponent implements OnChanges {
   @Input() message: string;
 
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.clearMessage();
+  }
+
+  clearMessage() {
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
 }
